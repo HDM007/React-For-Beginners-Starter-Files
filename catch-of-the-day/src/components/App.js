aa2cc3d0b1dee4932e1d6496b45f69faee4d1a34
hdm007 <harry.mardirossian13@gmail.com>
2021-01-03 07:26:27 -0500

    Staging for production

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from "prop-types";
 import Header from './Header';
 import Order from "./Order"
 import Inventory from "./Inventory"
@@ -12,11 +13,14 @@ class App extends React.Component{
         order: {}
     };
 
+    static propTypes = {
+        match: PropTypes.object
+    }
+
     componentDidMount(){
         const { params } = this.props.match;
         // first reinstate our localStorage
         const localStorageRef = localStorage.getItem(params.storeID);
-        console.log(localStorageRef)
         if(localStorageRef){
             this.setState({ order: JSON.parse(localStorageRef)})
         }
@@ -28,7 +32,6 @@ class App extends React.Component{
     }
 
     componentDidUpdate() {
-        console.log(this.state.order);
         localStorage.setItem(this.props.match.params.storeID, JSON.stringify(this.state.order));
     }
 
@@ -46,6 +49,24 @@ class App extends React.Component{
 
     };
 
+    updateFish = (key, updatedFish) => {
+        //1 take copy of current state
+        const fishes = {...this.state.fishes};
+        //2 update that state
+        fishes[key] = updatedFish;
+        //3 set to state
+        this.setState({fishes})
+    }
+
+    deleteFish = (key) => {
+        //1. take copy of current state
+        const fishes = {...this.state.fishes};
+        //2 update the state
+        fishes[key] = null;
+        //3 update state
+        this.setState({ fishes });
+    };
+
     loadSampleFishes= () =>{
         this.setState({fishes: sampleFishes})
     };
@@ -58,6 +79,14 @@ class App extends React.Component{
         this.setState({ order });
     };
 
+    removeFromOrder = key => {
+        //take a copy of state
+        const order = {...this.state.order};
+        //Remove from order
+        delete order[key];
+        this.setState({ order });
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -67,8 +96,8 @@ class App extends React.Component{
                         {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order}/>
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+                <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder}/>
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} updateFish={this.updateFish} deleteFish={this.deleteFish} storeID={this.props.match.params.storeID}/>
             </div>
         )
     }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,11 +1,82 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import firebase from 'firebase';
 import AddFishForm from './AddFishForm';
+import EditFishForm from  './EditFishForm'
+import Login from './Login';
+import base, { firebaseApp } from "../base";
+
 
 class Inventory extends React.Component{
+    static propTypes = {
+        fishes: PropTypes.object,
+        updateFish: PropTypes.func,
+        deleteFish: PropTypes.func,
+        loadSampleFishes: PropTypes.func
+    };
+
+    state = {
+        uid: null,
+        owner: null
+    }
+
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user =>{
+            if(user){
+                this.authHandler({user})
+            }
+        })
+    }
+
+    authHandler = async (authData) => {
+        //look up current store in firebase, and claim if no owner.
+        const store = await base.fetch(this.props.storeID, {context: this});
+        console.log(store)
+        if (!store.owner){
+            //save it as our own
+            await base.post(`${this.props.storeID}/owner`, {data: authData.user.uid
+            });
+        }
+        this.setState({
+            uid: authData.user.uid,
+            owner: store.owner || authData.user.uid
+        });
+
+        console.log(authData)
+    }
+
+    authenticate = provider => {
+        const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+        firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+    }
+
+    logout = async () => {
+        console.log("Logging out!");
+        await firebase.auth().signOut();
+        this.setState({uid: null});
+
+    }
+
     render() {
+        const logout = <button onClick={this.logout}>Log Out!</button>;
+        // Check if logged in
+        if(!this.state.uid){
+            return <Login authenticate={this.authenticate}/>
+        }
+        //Check ownership
+        if(this.state.uid !== this.state.owner){
+            return <div>
+                <h2>Inventory Login</h2>
+                <p>Sorry, you're not the owner.</p>
+                {logout}
+            </div>
+        }
+        //Finally, they must be owner, so just render the below inventory.
         return (
             <div className="inventory">
                 <h2>Inventory!!</h2>
+                {logout}
+                {Object.keys(this.props.fishes).map(key => <EditFishForm key={key} index={key} fish={this.props.fishes[key]} updateFish={this.props.updateFish} deleteFish={this.props.deleteFish}/>)}
                 <AddFishForm addFish={this.props.addFish}/>
                 <button onClick={this.props.loadSampleFishes} >Load Sample Fishes</button>
             </div>
